c7bab2000728daa2f3d2e37bf1ce67d3026dc80e
Todor Mitskovski <todorbm85@gmail.com>
2019-07-18 08:11:45 +0300

    Initial

---
+++ MVC_Forums/Mvc/Controllers/ThreadsController.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Telerik.Sitefinity.Forums;
+using Telerik.Sitefinity.Forums.Model;
+using Telerik.Sitefinity.Frontend.Mvc.Helpers;
+using Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers;
+using Telerik.Sitefinity.Frontend.Mvc.Models;
+using Telerik.Sitefinity.Model;
+using Telerik.Sitefinity.Modules.Pages.Configuration;
+using Telerik.Sitefinity.Mvc;
+
+namespace MVC_Forums.Mvc.Controllers
+{
+    [ControllerToolboxItem(Name = "ForumThreads_MVC", Title = "Forum Threads", SectionName = ToolboxesConfig.ContentToolboxSectionName, ModuleName = "Forums")]
+    public class ThreadsController : ContentBaseController
+    {
+        // TODO: designer with selector for forum or perhaps not needed - app will have only one forum, we need to list only threads with their posts
+        public string ForumName { get; set; }
+
+        public ActionResult Index(int? page)
+        {
+            var pageNumber = this.ExtractValidPage(page);
+            var items = this.GetThreads().Skip((pageNumber - 1) * 50).Select(x => new ItemViewModel(x));
+
+            var viewModel = new ContentListViewModel()
+            {
+                Items = items.ToList()
+            };
+
+            // TODO: Cache dependencies?
+
+            return this.View("List.ForumThreadsList", viewModel);
+        }
+
+        public ActionResult Details(ForumThread item)
+        {
+            this.ViewBag.ThreadTitle = item.Title;
+            this.InitializeMetadataDetailsViewBag(item);  // Sitefinity SEO optimizations
+            var posts = this.GetPosts(item).Select(x => new ItemViewModel(x));
+
+            var viewModel = new ContentListViewModel()
+            {
+                Items = posts.ToList()
+            };
+
+            return this.View("Detail.ForumThreadDetails", viewModel);
+        }
+
+        private IQueryable<ForumThread> GetThreads()
+        {
+            // TODO: provider
+            return ForumsManager.GetManager().GetThreads().Where(x => x.Forum.Title == this.ForumName);
+        }
+
+        private IQueryable<ForumPost> GetPosts(ForumThread thread)
+        {
+            // TODO: provider
+            return ForumsManager.GetManager().GetPosts(thread.Id);
+        }
+    }
+}
---
+++ MVC_Forums/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MVC_Forums")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVC_Forums")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e93834ad-9271-4fed-b611-e90b47699477")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ControllerContainer]
+
+[assembly: Telerik.Sitefinity.Frontend.Mvc.Infrastructure.Controllers.Attributes.ResourcePackage]
